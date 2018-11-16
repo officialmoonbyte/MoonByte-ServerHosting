6b4b94e58300202d685890fb3505758aebdba869
alex ritter <braydelritter@gmail.com>
2018-11-16 02:17:08 -0600

    Need to commit

--- MoonByte ServerHosting Client/MoonByte ServerHosting Client/Controls/ServerGUI.cs
+++ MoonByte ServerHosting Client/MoonByte ServerHosting Client/Controls/ServerGUI.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Net;
 using System.Windows.Forms;
 
 namespace MoonByte.ClientSoftware.ServerHostingClient.Controls
@@ -8,7 +9,37 @@ namespace MoonByte.ClientSoftware.ServerHostingClient.Controls
         public ServerGUI(string Name)
         {
             this.Size = new System.Drawing.Size(180, 240);
-            this.BackColor = Color.Black;
         }
+
+        #region OnPaint
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            //Drawing background
+            DrawBackground(e);
+
+        }
+
+        #region Draw Background
+
+        private void DrawBackground(PaintEventArgs e)
+        {
+            var request = WebRequest.Create("");
+
+            Image background_Image = null;
+            using (var response = request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            {
+                background_Image = Bitmap.FromStream(stream);
+            }
+
+
+        }
+
+        #endregion
+
+        #endregion
     }
 }
